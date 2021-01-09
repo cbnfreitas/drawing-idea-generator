4271ecb1ccad0f0470cdc1a156d5464ac2565c4e
cbnfreitas <cbnfreitas@gmail.com>
2021-01-09 17:07:17 +0000

    .

--- backend/app/tests/routers/test_users_me_router.py
+++ backend/app/tests/routers/test_users_me_router.py
@@ -1,14 +1,17 @@
 from typing import Dict
 
+from app.routers.users_me_router import (
+    read_current_user_info_from_access_token, update_user_from_access_token)
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
 from ...core import route_paths, s
-from ..utils import is_success_code_response, random_lower_string
+from ..utils import is_success_code_response, random_lower_string, see_also
 from ..utils.auth_utils import get_access_token_from_email
 from ..utils.user_utils import create_or_update_user_via_service
 
 
+@see_also(read_current_user_info_from_access_token)
 def test_read_users_me_admin(
         client: TestClient, admin_token_headers: Dict[str, str]
 ) -> None:
@@ -22,6 +25,7 @@ def test_read_users_me_admin(
     assert current_user["email"] == s.FIRST_ADMIN
 
 
+@see_also(read_current_user_info_from_access_token)
 def test_read_users_me_normal(client: TestClient, db: Session) -> None:
 
     user = create_or_update_user_via_service(db)
@@ -36,6 +40,7 @@ def test_read_users_me_normal(client: TestClient, db: Session) -> None:
     assert current_user["email"] == user.email
 
 
+@see_also(update_user_from_access_token)
 def test_update_me_full_name(
         client: TestClient, user_token_headers: Dict[str, str]
 ) -> None:
