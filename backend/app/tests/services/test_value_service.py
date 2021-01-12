210d285d4a82952fea7fa421c77e9a941a19bde1
cbnfreitas <cbnfreitas@gmail.com>
2021-01-12 14:19:37 +0000

    .

--- backend/app/tests/services/test_value_service.py
+++ backend/app/tests/services/test_value_service.py
@@ -1,12 +1,12 @@
 import pytest
 
-from ...services import feature_service
-from ..utils.feature_utils import (create_random_feature_schema,
-                                   create_random_feature_with_service)
+from ...services import value_service
+from ..utils.value_utils import (create_random_value_schema,
+                                 create_random_value_with_service)
 from .test_base_service import _TestBaseService
 
 
 @pytest.mark.parametrize("entity_service, create_random_entity_schema, create_random_entity_with_service",
-                         [(feature_service, create_random_feature_schema, create_random_feature_with_service)])
+                         [(value_service, create_random_value_schema, create_random_value_with_service)])
 class TestValueService(_TestBaseService):
     pass
