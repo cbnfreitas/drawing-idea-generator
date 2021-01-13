0e0440fa438df8d480f73a8d1cd8b5917169e1e9
cbnfreitas <cbnfreitas@gmail.com>
2021-01-13 19:20:15 +0000

    .

--- backend/app/core/baseline.py
+++ backend/app/core/baseline.py
@@ -1,10 +1,15 @@
 from app.core import s
+from app.models.feature_model import FeatureModel
+from app.models.value_model import ValueModel
+from app.schemas.feature_schema import FeatureCreateSchema
+from app.schemas.value_schema import ValueCreateSchema
 from fastapi import Depends
+from sqlalchemy import and_
 from sqlalchemy.orm import Session
 
 from ..models.base_model import BaseModel
 from ..schemas.user_schema import UserCreateSchemaAdmin
-from ..services import user_service
+from ..services import feature_service, user_service, value_service
 from .depends import get_db
 from .session import engine
 
@@ -32,8 +37,27 @@ def baseline(db: Session = Depends(get_db)) -> None:
         )
         user = user_service.create(db, obj_in=user_in)
 
+    initial_features_and_values = [
+        {"feature": "Raça", "values":  ["Humano", "Elfo", "Alien"]},
+        {"feature": "Identidade de gênero", "values": [
+            "Feminino", "Masculino", "Outros"]},
+        {"feature": "Ação", "values": ["Correndo", "Dormindo", "Estudando"]},
+        {"feature": "Como", "values": ["Feliz", "Cansado", "Zangado"]},
+    ]
 
-# Raça: Humano, Elfo, Alien
-# Identidade de gênero: Masculino, Feminino, Outros
-# Ação: Correndo, comendo, estudando
-# Onde: Feliz, com medo, cansado
+    for initial_feature in initial_features_and_values:
+        feature_models = feature_service.read_many(db, criteria=(
+            FeatureModel.name == initial_feature["feature"]))
+        if feature_models:
+            feature_id = feature_models[0].id
+        else:
+            feature_model = feature_service.create(
+                db, obj_in=FeatureCreateSchema(name=initial_feature["feature"]))
+            feature_id = feature_model.id
+
+        for value in initial_feature["values"]:
+            value_models = value_service.read_many(db, criteria=(
+                and_(ValueModel.feature_id == feature_id, ValueModel.value == value)))
+            if not value_models:
+                value_model = value_service.create(
+                    db, obj_in=ValueCreateSchema(value=value, feature_id=feature_id))
